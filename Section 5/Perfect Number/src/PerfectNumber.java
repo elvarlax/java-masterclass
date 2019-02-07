54dd1788085b8999243ed1e7211e9322a64ec198
Elvar Laxdal <elvarlax@gmail.com>
2019-02-07 15:08:42 +0000

    Adding Exercise 22

---
+++ Section 5/Perfect Number/src/PerfectNumber.java
@@ -0,0 +1,23 @@
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number <= 0) {
+            return false;
+        }
+        
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+        return sum == number;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+        System.out.println(isPerfectNumber(0));
+    }
+}
