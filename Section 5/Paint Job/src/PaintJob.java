1e6137be94978f078f40f3f0ef9abd0010d3a8b8
Elvar Laxdal <elvarlax@gmail.com>
2019-02-12 22:27:52 +0000

    Coding Exercise 30

--- Section 5/Paint Job/src/PaintJob.java
+++ Section 5/Paint Job/src/PaintJob.java
@@ -25,19 +25,19 @@ public class PaintJob {
     }
 
     public static void main(String[] args) {
-        //System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
-        //System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
-        //System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
-        //System.out.println(getBucketCount(0.0, 2.1, 1.5, 7));
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
+        System.out.println(getBucketCount(0.0, 2.1, 1.5, 7));
         System.out.println(getBucketCount(0.75, 0.75, 0.5, 0));
         System.out.println(getBucketCount(1.4, 2.1, 1.5, -4));
-        //System.out.println();
-        //System.out.println(getBucketCount(-3.4, 2.1, 1.5));
-        //System.out.println(getBucketCount(3.4, 2.1, 1.5));
-        //System.out.println(getBucketCount(7.25, 4.3, 2.35));
-        //System.out.println();
-        //System.out.println(getBucketCount(3.4, 1.5));
-        //System.out.println(getBucketCount(6.26, 2.2));
-        //System.out.println(getBucketCount(3.26, 0.75));
+        System.out.println();
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+        System.out.println();
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, 0.75));
     }
 }
---
+++ Section 6/Person/src/Person.java
@@ -0,0 +1,61 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0 || age > 100) this.age = 0;
+        else this.age = age;
+    }
+
+    public String getFullName() {
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return "";
+        } else if (firstName.isEmpty()) {
+            return lastName;
+        } else if (lastName.isEmpty()) {
+            return firstName;
+        } else {
+            return firstName + " " + lastName;
+        }
+    }
+
+    public boolean isTeen() {
+        return age > 12 && age < 20;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFirstName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");
+        System.out.println("fullName= " + person.getFullName());
+    }
+}
