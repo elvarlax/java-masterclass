a7d7c40639d87d240795bf995c6175cc0ee827a3
Elvar Laxdal <elvarlax@gmail.com>
2019-02-13 00:48:54 +0000

    Coding Exercise 32

---
+++ Section 6/Point/src/Point.java
@@ -0,0 +1,50 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance() {
+        return Math.sqrt((0 - x) * (0 - x) + (0 - y) * (0 - y));
+    }
+
+    public double distance(int x, int y) {
+        return Math.sqrt((getX() - x) * (getX() - x) + (getY() - y) * (getY() - y));
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt((p.x - x) * (p.x - x) + (p.y - y) * (p.y - y));
+    }
+
+    public static void main(String[] args) {
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+        Point point = new Point();
+        System.out.println("distance()= " + point.distance());
+    }
+}
