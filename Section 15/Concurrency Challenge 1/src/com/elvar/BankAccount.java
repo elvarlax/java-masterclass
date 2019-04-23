57e7978b43606345f6376358f8a7a49b16bcae84
Elvar Laxdal <elvarlax@gmail.com>
2019-04-23 17:33:43 +0000

    Concurrency Challenge 1

---
+++ Section 15/Concurrency Challenge 1/src/com/elvar/BankAccount.java
@@ -0,0 +1,24 @@
+package com.elvar;
+
+class BankAccount {
+
+    private double balance;
+    private String accountNumber;
+
+    public BankAccount(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ Section 15/Concurrency Challenge 1/src/com/elvar/Main.java
@@ -0,0 +1,33 @@
+package com.elvar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        We could have two people using a joint bank account at the same time.
+        Create and start two threads that use the same BankAccount instance and an initial balance of $1000.00.
+        One will deposit $300.00 into the bank account, and then withdraw $50.00.
+        The other will deposit $203.75 and then withdraw $100.00
+        */
+        final BankAccount account = new BankAccount("12345-678", 1000.00);
+
+        Thread t1 = new Thread() {
+            public void run() {
+                account.deposit(300.00);
+                account.withdraw(50.00);
+                System.out.println(account.getBalance());
+            }
+        };
+
+        Thread t2 = new Thread() {
+            public void run() {
+                account.deposit(203.75);
+                account.withdraw(100.00);
+                System.out.println(account.getBalance());
+            }
+        };
+
+        t1.start();
+        t2.start();
+    }
+}
